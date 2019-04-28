39c6509dd4b787a5e2f3e7a26388da226a7658eb
Maciej Krawiecki <m.a.krawiecki@gmail.com>
2019-04-28 22:43:34 +0200

    added test to member dao method get all

--- src/test/java/maciejkrawiecki/daoimpl/MemberDaoImplTest.java
+++ src/test/java/maciejkrawiecki/daoimpl/MemberDaoImplTest.java
@@ -5,6 +5,8 @@ import maciejkrawiecki.entity.Member;
 import org.junit.Test;
 
 import javax.persistence.NoResultException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
@@ -89,5 +91,44 @@ public class MemberDaoImplTest {
     @Test
     public void getAll() {
 
+        // given
+
+        MemberDao memberDao = new MemberDaoImpl();
+
+        Member member1 = new Member();
+        Member member2 = new Member();
+
+        member1.setStartNumber(1);
+        member1.setLastName("lastname1");
+        member1.setName("name1");
+
+        member2.setStartNumber(2);
+        member2.setLastName("lastname2");
+        member2.setName("name2");
+
+        memberDao.save(member1);
+        memberDao.save(member2);
+
+        List<Member> members = new ArrayList<>();
+        Member firstMember = new Member();
+        Member secondMember = new Member();
+
+        // when
+
+        members = memberDao.getAll();
+
+        firstMember = members.get(0);
+        secondMember = members.get(1);
+
+        // then
+
+        assertEquals("lastname1", firstMember.getLastName());
+        assertEquals("name1", firstMember.getName());
+        assertEquals(1, firstMember.getStartNumber());
+
+
+        assertEquals("lastname2", secondMember.getLastName());
+        assertEquals("name2", secondMember.getName());
+        assertEquals(2, secondMember.getStartNumber());
     }
 }
